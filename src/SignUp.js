22063397b94d09b93f5c8b9ba529375ef53f6028
Riya <68336293+Riya500-git@users.noreply.github.com>
2023-08-13 13:33:34 +0530

    Update SignUp.js

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,40 +1,48 @@
 import React from 'react';
-import { Box, Center, Heading, Text, Flex, Input, Link, Button, ChakraProvider } from '@chakra-ui/react';
+import { Box, Center, Heading, Text, Flex, Input, Link, Button, ChakraProvider, Image } from '@chakra-ui/react';
+import logo from './Logo (1).png'
 
 const SignupPage = () => {
   return (
     <ChakraProvider>
+      <Box p={4} bg="white" borderRadius="md">
+        <Image src={logo} alt="Logo" w={8} h={8} />
+      </Box>
     <Center margin="100px">
-    <Box p={4} bg="gray.100" borderRadius="md" maxW="500">
+    <Box p={4} bg="white" borderRadius="md" maxW="500">
       <Center>
-        <Heading size="md">Let’s get started</Heading>
+        <Heading size="xl">Let’s get started</Heading>
       </Center>
       <Text mt={4} textAlign="center">
       Create an account to start digitizing your identity.
       </Text>
       <Flex mt={6}>
         <Box mr={2}>
-        <Text mt={4}>First Name</Text>
+        <Text mt={4} textAlign="left">First Name</Text>
           <Input placeholder="First Name" />
         </Box>
         <Box ml={2}>
-        <Text mt={4}>Last Name</Text>
+        <Text mt={4} textAlign="left">Last Name</Text>
           <Input placeholder="Last Name" />
         </Box>
       </Flex>
-      <Text mt={4}>Email</Text>
+      <Text mt={4} textAlign="left">Email</Text>
       <Input placeholder="Email" />
-      <Text mt={4}>Password</Text>
+      <Text mt={4} textAlign="left">Password</Text>
       <Input type="password" placeholder="Password" />
       <Flex mt={4} justifyContent="space-between">
         <Link>Remember me</Link>
         <Link>Forgot password?</Link>
       </Flex>
-      <Button mt={6} colorScheme="blue" size="lg" isFullWidth>
+      <Button mt={6} colorScheme="blue" size="lg" width="400px">
         Sign Up Now
       </Button>
     </Box>
     </Center>
+
+    <Box mt={12} textAlign="center">
+          <Text>Contact Us</Text>
+        </Box>
     </ChakraProvider>
   );
 };
